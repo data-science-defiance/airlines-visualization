690a27d2ff13ab0258035cc70d6796122d80f343
Sean Takafuji <seanytak@hawaii.edu>
2018-11-22 12:06:27 -1000

    Updates

---
+++ precache-manifest.2719a7455f35760d77f6a077c99f564a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/airlines-visualization/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1eb840d3e867a4c0e47c",
+    "url": "/airlines-visualization/static/js/runtime~main.1eb840d3.js"
+  },
+  {
+    "revision": "b1fa177d883733aedd9c",
+    "url": "/airlines-visualization/static/js/main.b1fa177d.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/airlines-visualization/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "b1fa177d883733aedd9c",
+    "url": "/airlines-visualization/static/css/main.0e9292a7.chunk.css"
+  },
+  {
+    "revision": "c10140326b7d92b94a0007daad6e9772",
+    "url": "/airlines-visualization/index.html"
+  }
+];
