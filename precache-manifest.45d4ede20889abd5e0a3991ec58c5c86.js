7b3b07dced5b583bd6344d8a819871ed01d578b1
Sean Takafuji <seanytak@hawaii.edu>
2018-12-04 22:47:18 -1000

    Updates

---
+++ precache-manifest.45d4ede20889abd5e0a3991ec58c5c86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f4020c9f9bc829f6dcb4c5879ad5c56",
+    "url": "/airlines-visualization/static/media/takeoff.3f4020c9.svg"
+  },
+  {
+    "revision": "b5f7e43beda6c256fa11f1690d99c465",
+    "url": "/airlines-visualization/static/media/arriving.b5f7e43b.svg"
+  },
+  {
+    "revision": "1eb840d3e867a4c0e47c",
+    "url": "/airlines-visualization/static/js/runtime~main.1eb840d3.js"
+  },
+  {
+    "revision": "033e100bda114dba6719",
+    "url": "/airlines-visualization/static/js/main.033e100b.chunk.js"
+  },
+  {
+    "revision": "3aed4d9c11d1b722b0a5",
+    "url": "/airlines-visualization/static/js/1.3aed4d9c.chunk.js"
+  },
+  {
+    "revision": "033e100bda114dba6719",
+    "url": "/airlines-visualization/static/css/main.b7de8710.chunk.css"
+  },
+  {
+    "revision": "3aed4d9c11d1b722b0a5",
+    "url": "/airlines-visualization/static/css/1.15f23f5f.chunk.css"
+  },
+  {
+    "revision": "5d795319d64205770beba45686dc3ec2",
+    "url": "/airlines-visualization/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/airlines-visualization/precache-manifest.0238f013842d226317dfac0d7fefe9dc.js"
+  "/airlines-visualization/precache-manifest.45d4ede20889abd5e0a3991ec58c5c86.js"
 );
 
 workbox.clientsClaim();
